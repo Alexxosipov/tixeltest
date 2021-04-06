01d5395e785570d9ebebeb5eb49b387513804bd4
alexxosipov <alexxosipov@yandex.ru>
2021-04-06 23:52:34 +0800

    start.sh edited

--- start.sh
+++ start.sh
@@ -1,2 +1,3 @@
 cp .env.example .env
-cd docker && docker-compose up --build
+cd docker && docker-compose up --build -d
+docker exec -i tixel-php-fpm bash -c "cd /application && php artisan key:generate"
